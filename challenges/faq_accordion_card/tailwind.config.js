a27102e3d0037e37186198bf1abbbdb4dbd4a05e
Dean Kinyua <kinyuadean@gmail.com>
2025-01-16 21:16:59 +0300

    added active states for the label which opens the paragraph

--- challenges/faq_accordion_card/tailwind.config.js
+++ challenges/faq_accordion_card/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
         'custom-blue': '#67E2E9',
-        'custom-red': 'hsl(14, 88%, 65%)',
       },
     },
   },
